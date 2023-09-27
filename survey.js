d259056a4e6b2bddc5f53d0bc547d5d48ef7d62c
Alanna <phan.alanna@gmail.com>
2023-09-27 19:03:11 -0400

    added semicolons

--- survey.js
+++ survey.js
@@ -12,12 +12,12 @@ rl.question("What's your name? Nicknames are also acceptable :) ", (answer0) => 
         rl.question("What's your favourite thing to eat for that meal? ", (answer4) => {
           rl.question("Which sport is your absolute favourite? ", (answer5) => {
             rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer6) => {
-              console.log(`${answer0} loves to listen to ${answer2} while doing their favourite activity, ${answer1}. ${answer0}'s favourite thing to eat for ${answer3} is ${answer4}. Their favourite sport is ${answer5} and they're amazing at ${answer6}!`)
+              console.log(`${answer0} loves to listen to ${answer2} while doing their favourite activity, ${answer1}. ${answer0}'s favourite thing to eat for ${answer3} is ${answer4}. Their favourite sport is ${answer5} and they're amazing at ${answer6}!`);
               rl.close();
-            })
-          })
-        })
-      })
-    })
-  })
+            });
+          });
+        });
+      });
+    });
+  });
 });
